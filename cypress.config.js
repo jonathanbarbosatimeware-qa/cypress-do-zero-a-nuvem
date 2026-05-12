a040d10bab4abba6072cf2fb17a4741c5514dc0f
Jonathan Barbosa <jonathanbarbosa.timeware@gmail.com>
2026-05-12 20:11:40 -0300

    fix: atualiza record key e project id para o cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'd6yi5d',
+  projectId: 'wd3cv2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
